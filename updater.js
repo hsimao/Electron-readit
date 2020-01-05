ddba3dc4bf38716176678fc9d442338fc6ca15cf
hsimao <e087754958@gmail.com>
2020-01-05 16:54:18 +0800

    add: 監聽下載進度, 並詢問用戶是否要安裝並重啟

--- updater.js
+++ updater.js
@@ -13,7 +13,7 @@ module.exports = () => {
   console.log('checking for updates');
   autoUpdater.checkForUpdates();
 
-  // 監聽 update
+  // 監聽是否可以更新
   autoUpdater.on('update-available', () => {
     // 彈窗詢問用戶是否下載
     dialog.showMessageBox(
@@ -26,6 +26,22 @@ module.exports = () => {
       buttonIndex => {
         // 如果點選'更新', 就調用下載更新方法
         if (buttonIndex === 0) autoUpdater.downloadUpdate();
+      }
+    );
+  });
+
+  // 監聽更新下載
+  autoUpdater.on('update-downloaded', () => {
+    // 下載完成時詢問用戶是否要重啟
+    dialog.showMessageBox(
+      {
+        type: 'info',
+        title: '下載完成',
+        message: '是否要馬上安裝並重啟？',
+        buttons: ['安裝並重啟', '先不用']
+      },
+      buttonIndex => {
+        if (buttonIndex === 0) autoUpdater.quitAndInstall(false, true);
       }
     );
   });
